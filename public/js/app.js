6aab132cef6346e157200c6abace14fcc326567a
kashyap <kashyap.amipara@kevit.io>
2022-06-27 15:19:22 +0530

    Set up app

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit',(e)=>{
 
     m1.textContent='Loading...';
     m2.textContent='';
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         console.log(data);
         if(data.error)m1.textContent=data.error;
